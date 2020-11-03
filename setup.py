7f95c11f8152481a726b62220c0e60b1be65508b
Nikola Milosevic <nmilosevic@cbs.mpg.de>
2020-11-03 19:09:19 +0100

    test environment

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    readme_text = fh.read()
+
+setup(
+    name='forknet',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/milosen/forknet',
+    author='Nikola Milosevic',
+    author_email='milosevic.nikola@protonmail.com',
+    long_description=readme_text,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+)
---
+++ test/hello_world.py
@@ -0,0 +1,4 @@
+from forknet.test import test
+
+if __name__ == '__main__':
+    test()
